5acb6ed0a947f50d00cf67d2f07f80568fe8150e
Kelvin Modesto <km.modesto@gmail.com>
2019-01-15 13:18:02 -0300

    Refactoring model use and api structure

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,24 @@
 const { Router } = require('express');
+
 const MongoDB = require('../db/strategies/mongodb/mongoDBStrategy');
+const Context = require('../db/strategies/base/contextStrategy');
+
+// controllers list
+const ToolsController = require('../controllers/ToolsController');
+
+// models list
 const { Tool } = require('../models/tool');
-const ToolController = require('../controllers/ToolController');
 
-const routesInit = () => {
+// Put all models in a single object
+const models = { Tool };
 
+const context = new Context(MongoDB.connect(), models.Tool);
+const routersInit = () => {
+  const toolsController = new ToolsController(Router(), context);
+
+  toolsController.router.use('/tools', toolsController.init());
+
+  return toolsController.router;
 };
+
 module.exports = routersInit;
