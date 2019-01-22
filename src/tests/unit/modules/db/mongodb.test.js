2e6fec7e207355e9724c16d477c111d90bd2f85f
Kelvin Modesto <km.modesto@gmail.com>
2019-01-21 23:34:56 -0300

    create test case

--- src/tests/unit/modules/db/mongodb.test.js
+++ src/tests/unit/modules/db/mongodb.test.js
@@ -1,13 +1,22 @@
 const { describe } = require('mocha');
 const { expect } = require('chai');
-const sinon = require('sinon');
 
 const Context = require('../../../../db/strategies/base/contextStrategy');
 const MongoDB = require('../../../../db/strategies/mongodb/mongoDBStrategy');
 const { Tool } = require('../../../../models/tool');
 
-const MOCK_TOOLS_CREATE = {
-
+const MOCK_CREATE_TOOL = {
+  title: 'json-server',
+  link: 'https://github.com/typicode/json-server',
+  description: 'Fake REST API based on a json schema. Useful for mocking and creating APIs for front-end devs to consume in coding challenges.',
+  tags: [
+    'api',
+    'json',
+    'schema',
+    'node',
+    'github',
+    'rest',
+  ],
 };
 
 let context = {};
@@ -21,8 +30,19 @@ describe('MongoDB Test Suit', function init() {
   });
 
   it('Create item', async () => {
-    const { id, description, link, title, tags } = context.create();
-    expect(1).to.be.equal(1);
+    const {
+      title,
+      link,
+      description,
+      tags,
+    } = await context.create(MOCK_CREATE_TOOL);
+
+    expect({
+      title,
+      link,
+      description,
+      tags,
+    }).to.be.equal(MOCK_CREATE_TOOL);
   });
 
   it('Read item', async () => {
