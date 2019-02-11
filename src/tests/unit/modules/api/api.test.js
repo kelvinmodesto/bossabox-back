6e49a43d615250992586fa9970058f7a4cfd620e
Kelvin Modesto <km.modesto@gmail.com>
2019-02-10 23:49:51 -0300

    Refactoring test suit

--- src/tests/unit/modules/api/api.test.js
+++ src/tests/unit/modules/api/api.test.js
@@ -4,67 +4,57 @@ const chaiHttp = require('chai-http');
 
 const app = require('../../../../../app');
 
+const MOCK_CREATE_TOOL = {
+  title: 'json-server',
+  link: 'https://github.com/typicode/json-server',
+  description: 'Fake REST API based on a json schema. Useful for mocking and creating APIs for front-end devs to consume in coding challenges.',
+  tags: [
+    'api',
+    'json',
+    'schema',
+    'node',
+    'github',
+    'rest',
+  ],
+};
+
 chai.use(chaiHttp);
 
-describe.only('API Test Suit', function init() {
+describe('API Test Suit', function init() {
   this.beforeAll(async () => {
   });
 
-  it('API is listening the port 3000', async () => {
-    chai.expect(1).to.be.equal(1);
+  describe('Connect test case', async () => {
+    it('API is listening the port 3000', async () => {
+      chai.expect(1).to.be.equal(1);
+    });
   });
 
-  it('list all tools, without params', async () => {
-    chai.request(app)
-      .get('/tools')
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('array');
-        res.body.length.should.be.eql(0);
-      });
+  describe('/GET tools', async () => {
+    it('/GET tools', async () => {
+      chai.request(app)
+        .get('/tools')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          res.body.length.should.be.eql(0);
+        });
+    });
   });
-
-  it('filter tools with params', async () => {
-    chai.expect(1).to.be.equal(1);
+  describe('/POST tools', async () => {
+    it('/POST tools', async () => {
+      chai.request(app)
+        .post('/tools')
+        .send(MOCK_CREATE_TOOL)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+        });
+    });
   });
-
-  it('filter tools with tags params', async () => {
-    chai.expect(1).to.be.equal(1);
-  });
-
-  it('filter tools with all params', async () => {
-    chai.expect(1).to.be.equal(1);
-  });
-
-  it('filter tools with a invalid param', async () => {
-    chai.expect(1).to.be.equal(1);
-  });
-
-  it('create tool', async () => {
-    chai.expect(1).to.be.equal(1);
-  });
-
-  it('create a invalid tool', async () => {
-    chai.expect(1).to.be.equal(1);
-  });
-
-  it('create a empty tool', async () => {
-    chai.expect(1).to.be.equal(1);
-  });
-
-  it('delete tool', async () => {
-    chai.expect(1).to.be.equal(1);
-  });
-
-  it('delete a inexistent tool', async () => {
-    chai.expect(1).to.be.equal(1);
-  });
-
-  it('delete a invalid tool', async () => {
-    chai.expect(1).to.be.equal(1);
-  });
-
-  it('update a tool', async () => {
-    chai.expect(1).to.be.equal(1);
+  describe('/DELETE/:id tools', async () => {
+    it('/DELETE tools', async () => {
+      chai.expect(1).to.be.equal(1);
+    });
   });
 });
